0dff51718de21da7d84156bf8b411dd4de251dc0
Bartosz Skuza <bartosz.skuza@droidsonroids.pl>
2018-03-26 16:52:28 +0200

    handle fetching data error

--- lib/features/characters/screens/list.dart
+++ lib/features/characters/screens/list.dart
@@ -1,65 +1,93 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:marvel_heroes/features/characters/models/character.dart';
-import 'package:marvel_heroes/features/characters/services/character.dart' as CharacterService;
+import 'package:marvel_heroes/features/characters/services/character.dart'
+    as CharacterService;
+import 'package:meta/meta.dart';
 
 class CharactersList extends StatefulWidget {
   @override
   createState() => new CharacterListState();
-  CharactersList({
-    @required this.characterSelectedCallback,
-    this.selected
-  });
+
+  CharactersList({@required this.characterSelectedCallback, this.selected});
 
   final ValueChanged<Character> characterSelectedCallback;
   final Character selected;
 }
 
 class CharacterListState extends State<CharactersList> {
+  final int _limit = 20;
+  final double _fetchBeforeEndOffset = 50.0;
   List<Character> _characters = [];
   bool _isMore = true;
   bool _fetching = true;
+  bool _initialized = false;
+  bool _error = false;
   int _page = 1;
-  final int _limit = 20;
+  ScrollController _controller = new ScrollController();
 
   @override
   void initState() {
     super.initState();
-    _fetchData();
+    _fetchData().whenComplete(() {
+      this.setState(() {
+        _initialized = true;
+      });
+    });
   }
 
-  void _fetchData() {
-    CharacterService.fetchAll(_page, _limit).then((response) {
+  Future _fetchData() {
+    return CharacterService.fetchAll(_page, _limit).then((response) {
       this.setState(() {
         _characters.addAll(response.data);
         _isMore = response.hasMore;
         _fetching = false;
         _page += 1;
+        _error = false;
+      });
+    }).catchError((_) {
+      this.setState(() {
+        _fetching = false;
+        _error = true;
       });
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    ScrollController _controller = new ScrollController();
+  Widget _buildError() {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        new Container(
+          child: new Text(
+            'Some error appears',
+            style: Theme.of(context).textTheme.body1,
+          ),
+          margin: const EdgeInsets.only(bottom: 8.0),
+        ),
+        _fetching
+            ? new CircularProgressIndicator()
+            : new RaisedButton(
+                onPressed: () {
+                  this.setState(() {
+                    _fetching = true;
+                  });
+                  _fetchData();
+                },
+                child: new Text('Retry fetch data')),
+      ],
+    );
+  }
 
-    _controller.addListener(() {
-      if (_controller.position.maxScrollExtent <= _controller.offset + 50.0) {
-        if (!_fetching && _isMore) {
-          this.setState(() {
-            _fetching = true;
-          });
-          _fetchData();
-        }
-      }
-    });
-
-    Widget _renderList() {
-      return new ListView.builder(
-        controller: _controller,
-        itemCount: _fetching ? _characters.length + 1 : _characters.length,
-        itemBuilder: (context, index) {
-          if (index >= _characters.length) {
+  Widget _buildList() {
+    return new ListView.builder(
+      controller: _controller,
+      itemCount:
+      _fetching || _error ? _characters.length + 1 : _characters.length,
+      itemBuilder: (context, index) {
+        if (index >= _characters.length) {
+          if (_fetching) {
             return new Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -70,27 +98,52 @@ class CharacterListState extends State<CharactersList> {
               ],
             );
           }
-          return new ListTile(
-            leading: new Container(
-              child: new CircleAvatar(
-                backgroundImage: new NetworkImage(_characters[index].thumbnail),
-              ),
-            ),
-            title: new Text(
-              _characters[index].name,
-              style: Theme.of(context).textTheme.body1,
-            ),
-            onTap: () =>widget.characterSelectedCallback(_characters[index]),
-            selected: widget.selected ==_characters[index],
+          return new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: _buildError(),
           );
-        },
-      );
-    }
+        }
+        return new ListTile(
+          leading: new Container(
+            child: new CircleAvatar(
+              backgroundImage: new NetworkImage(_characters[index].thumbnail),
+            ),
+          ),
+          title: new Text(
+            _characters[index].name,
+            style: Theme.of(context).textTheme.body1,
+          ),
+          onTap: () => widget.characterSelectedCallback(_characters[index]),
+          selected: widget.selected == _characters[index],
+        );
+      },
+    );
+  }
 
-    return new Scaffold(
-        body: new Center(
-            child: _fetching && _page == 1
-                ? new CircularProgressIndicator()
-                : _renderList()));
+  Widget _buildContent() {
+    if (_fetching && !_initialized) {
+      return new CircularProgressIndicator();
+    }
+    if (_error && _characters.length == 0) {
+      return _buildError();
+    }
+    return _buildList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    _controller.addListener(() {
+      if (_controller.position.maxScrollExtent <= _controller.offset + _fetchBeforeEndOffset) {
+        if (!_fetching && _isMore) {
+          this.setState(() {
+            _fetching = true;
+          });
+          _fetchData();
+        }
+      }
+    });
+
+    return new Scaffold(body: new Center(child: _buildContent()));
   }
 }
